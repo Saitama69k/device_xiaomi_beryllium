124248dff75fdf41717f390b1a7633275d3251c6
Saitama69k <123988811+Saitama69k@users.noreply.github.com>
2023-02-06 23:59:03 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/evolution_beryllium.mk
+    $(LOCAL_DIR)/voltage_beryllium.mk
 
 COMMON_LUNCH_CHOICES := \
-    evolution_beryllium-user \
-    evolution_beryllium-userdebug \
-    evolution_beryllium-eng
+    voltage_beryllium-user \
+    voltage_beryllium-userdebug \
+    voltage_beryllium-eng
