016efbde54ff2dc20cdd3feaa2bd9b5d8248ca9d
Saitama69k <123988811+Saitama69k@users.noreply.github.com>
2023-02-07 06:15:13 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -27,4 +27,4 @@ git clone https://github.com/ArrowOS/android_external_tinyxml -b arrow-13.0 exte
 
 #Clone Hardware
 rm -rf hardware/xiaomi
-git clone https://github.com/Saitama69k/hardware.git -b thirteen hardware/xiaomi
+git clone https://github.com/Saitama69k/hardware_xiaomi.git -b thirteen hardware/xiaomi
