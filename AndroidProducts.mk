22f81ae5661617617e9b00c91343f871fc57e1c0
Saitama69k <123988811+Saitama69k@users.noreply.github.com>
2023-03-08 14:07:01 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beryllium.mk
+    $(LOCAL_DIR)/bananadroid_beryllium.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_beryllium-user \
-    lineage_beryllium-userdebug \
-    lineage_beryllium-eng
+    bananadroid_beryllium-user \
+    bananadroid_beryllium-userdebug \
+    bananadroid_beryllium-eng
